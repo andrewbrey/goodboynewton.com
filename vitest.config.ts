01b2edec37eb66836a2d32b050dae4f56bb4e25d
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-03-26 19:44:46 -0700

    test(vitest): include __tests__ in addition to default includes

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,10 +4,14 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { configDefaults } from "vitest/config";
+
+const defaultInclude = configDefaults.include ?? [];
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
+    include: [...defaultInclude, "**/__tests__/**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     globals: true,
     environment: "happy-dom",
     setupFiles: ["./test/setup-test-env.ts"],
