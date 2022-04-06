e5aeadfa74ee3c03452fc9b5d275b3a7015eba0b
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-04-06 14:42:52 -0700

    feat(*): upgrade to react 18 and bump deps

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,12 @@
-import { renderToString } from "react-dom/server";
-import type { EntryContext } from "remix";
-import { RemixServer } from "remix";
+import { Headers, Response } from "@remix-run/node";
+import isbot from "isbot";
+import { PassThrough } from "node:stream";
+// TODO: remove ts-expect-error when @types are updated
+// @ts-expect-error
+import { renderToPipeableStream } from "react-dom/server";
+import { RemixServer, type EntryContext } from "remix";
+
+const ABORT_TIMEOUT = 5000;
 
 export default function handleRequest(
   request: Request,
@@ -8,12 +14,30 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const markup = renderToString(<RemixServer context={remixContext} url={request.url} />);
+  const callbackName = isbot(request.headers.get("user-agent")) ? "onAllReady" : "onShellReady";
 
-  responseHeaders.set("Content-Type", "text/html");
+  return new Promise((resolve) => {
+    let didError = false;
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+    const { pipe, abort } = renderToPipeableStream(<RemixServer context={remixContext} url={request.url} />, {
+      [callbackName]() {
+        const body = new PassThrough();
+
+        responseHeaders.set("Content-Type", "text/html");
+
+        resolve(
+          new Response(body, {
+            status: didError ? 500 : responseStatusCode,
+            headers: responseHeaders,
+          })
+        );
+        pipe(body);
+      },
+      onError(error: Error) {
+        didError = true;
+        console.error(error);
+      },
+    });
+    setTimeout(abort, ABORT_TIMEOUT);
   });
 }
