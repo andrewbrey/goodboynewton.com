c4010c4922c4001e327a4c99d7513c50c8a0fd3f
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2023-12-21 12:32:09 -0800

    feat: add error component

---
+++ src/routes/[...catchall]/+error.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+import UhOh from "$lib/assets/icon.png?enhanced=true&w=512&photo";
+</script>
+
+<main class="grid h-dvh place-items-center p-4">
+	<div class="flex flex-col items-center gap-4">
+		<enhanced:img
+			src={UhOh}
+			alt="illustration of Newton the golden retriever wearing a lobster costume that is too small for him while making a face like he's not sure what is going on"
+			sizes="(min-width:320px) 512px"
+			class="pointer-events-none max-w-[250px] select-none object-cover text-transparent"
+		/>
+		<h1 class="text-center font-display text-2xl">Whoops, sorry! Newton will be right back!</h1>
+	</div>
+</main>
